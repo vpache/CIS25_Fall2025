7bd3cf771c96d98064303324dd5bfe4e5e031f8c
vpache <victor.pacheco7579@gmail.com>
2025-08-22 19:09:30 -0700

    Add files via upload

---
+++ hello_inventory.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std; 
+int main() { 
+//cout prints to the screen, the line below will print Hello Inventory System
+	cout << "Hello Inventory System!\n"; 
+	cout << "\tData Types:\n"; 
+	cout << "Size of int: " << sizeof(int) << " bytes\n"; 
+	cout << "Size of float: " << sizeof(float) << " bytes\n"; 
+	cout << "Size of char: " << sizeof(char) << " bytes\n"; 
+	return 0; 
+} 
